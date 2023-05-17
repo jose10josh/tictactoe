39b6c1cf41617003d5db47e9e3c28f6295f1b456
José Arévalo <joseaf1002@gmail.com>
2023-05-17 15:09:06 +0200

    save to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,50 @@ import { Square } from "./components/Square";
 import { GameState, Turn } from "./constants/gameElements";
 import { checkEndGame, checkWinner } from "./utils/gameStatus";
 import { WinnerModal } from "./components/WinnerModal";
+import { saveGameToStorage, resetGameStorage } from "./utils/storage/index.js";
 import "./App.css";
 
 function App() {
-  const [Board, setBoard] = useState<string[]>(Array(9).fill(null));
-  const [turn, setTurn] = useState<string>(Turn.x);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? Turn.x;
+  });
   const [gameState, setGameState] = useState<GameState>(GameState.IN_PROGRESS);
 
   const updateBoard = (index: number) => {
     if (
-      Board[index] ||
+      board[index] ||
       gameState === GameState.DRAW ||
       gameState === GameState.WON
     )
       return;
 
-    const newBoard = [...Board];
+    const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
+
+    const newTurn = turn === Turn.x ? Turn.o : Turn.x;
+    setTurn(newTurn);
+
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn,
+    });
 
     const winner = checkWinner(newBoard);
     if (winner) {
       setGameState(GameState.WON);
       confetti();
-      return;
+      resetGameStorage();
     } else if (checkEndGame(newBoard)) {
       setGameState(GameState.DRAW);
-      return;
+      resetGameStorage();
     }
-
-    const newTurn = turn === Turn.x ? Turn.o : Turn.x;
-    setTurn(newTurn);
   };
 
   const handleReset = () => {
@@ -47,7 +60,7 @@ function App() {
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
-        {Board.map((item, index) => (
+        {board.map((item, index) => (
           <Square index={index} key={index} updateBoard={updateBoard}>
             <span className="cell__content">{item}</span>
           </Square>
---
+++ src/utils/storage/index.ts
@@ -0,0 +1,9 @@
+export const saveGameToStorage = ({ board, turn }:{ board: string[], turn: string }) => {
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', turn)
+}
+
+export const resetGameStorage = () => {
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
+}
